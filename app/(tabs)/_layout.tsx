2f8ebbdfe0ab30a3d5f169b47fcdb6e8797d1935
LeviTC <erletaco@outlook.com>
2026-02-27 13:51:27 -0600

    feat(profile): adding ui into profile screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,46 @@
 import { Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: { backgroundColor: '#111', borderTopColor: '#222' },
+        // tabBarShowLabel: false,
+        tabBarStyle: { 
+          backgroundColor: '#111', 
+          borderTopColor: '#222',
+        },
         tabBarActiveTintColor: '#00FF87',
-        tabBarInactiveTintColor: '#000',
+        tabBarInactiveTintColor: '#555',
+        tabBarLabelStyle: {
+          fontSize: 14
+        }
       }}
     >
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen 
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <AntDesign name='home' color={color} size={15} />
+          )
+        }}
+
+      />
+
+      <Tabs.Screen name="versus" options={{ title: 'Versus' }} />
+
+      <Tabs.Screen 
+        name="profile" 
+        options={{ 
+          title: 'Profile', 
+          tabBarIcon: ({color, size}) => (
+            <AntDesign name='user' color={color} size={15} />
+          )
+        }}  
+      />
     </Tabs>
   );
 }
